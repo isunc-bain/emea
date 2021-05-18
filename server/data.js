6d4035f1e79a0f8fa62bf43bae7711d7a6a92c71
Victor Chiu <isunvictor@gmail.com>
2021-05-18 23:30:54 +0200

    Set an id for each book imported from CSV

--- server/data.js
+++ server/data.js
@@ -4,7 +4,12 @@ const mCache = require('memory-cache');
 function loadBooksToCache() {
   csv()
     .fromFile(__dirname + '/../books.csv')
-    .then(books => mCache.put('books', books));
+    .then(books => {
+      books.map((book, i) => {
+        book.id = i;
+      });
+      mCache.put('books', books);
+    });
 }
 
 module.exports = loadBooksToCache;

da659378701c0c518d4ff1e5852c2c890d73f9a0
Victor Chiu <isunvictor@gmail.com>
2021-05-18 23:34:11 +0200

    Add route tests for book listing and book details page

--- server/routes/api/books.js
+++ server/routes/api/books.js
@@ -10,6 +10,24 @@ router.get('', async (req, res) => {
     res.send(books);
   } catch (error) {
     console.log(error);
+    res.status(500);
+    res.render('error', { error: error });
+  }
+});
+
+router.get('/:bookId', async (req, res) => {
+  try {
+    const bookId = req.params['bookId'];
+    if (bookId) {
+      const book = mCache.get('books')[bookId];
+      if (book) {
+        return res.send(book);
+      }
+    }
+    return res.status(404).send('Not found');
+  } catch (error) {
+    res.status(500);
+    res.render('error', { error: error });
   }
 });
 
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,13 +1,35 @@
-import { describe, it } from '@jest/globals';
+import { beforeAll, describe, it } from '@jest/globals';
+import loadBooksToCache from '../server/data';
 
 const supertest = require('supertest');
 const app = require('../server/app');
-const request = supertest(app);
+const request = supertest.agent(app);
 
-describe('Get pong', () => {
+beforeAll(async () => await loadBooksToCache());
+
+describe('Ping ', () => {
   it('should get a pong', async () => {
     const response = await request.get('/api/ping');
     expect(response.status).toBe(200);
     expect(response.text).toBe('pong');
+  });
+});
+
+describe('Books', () => {
+  it('should get books', async () => {
+    const response = await request.get('/api/books');
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(211);
+  });
+
+  it('should get book by id', async () => {
+    const response = await request.get('/api/books/1');
+    expect(response.status).toBe(200);
+    expect(response.body.id).toBe(1);
+  });
+
+  it('should get 404 if book id does not exist', async () => {
+    const response = await request.get('/api/books/9999');
+    expect(response.status).toBe(404);
   });
 });
